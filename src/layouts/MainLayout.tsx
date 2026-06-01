9b232dfdf8e9f8c0d830b0d6ac868a88ef1bf274
Chris.R <chrisrouty@gmail.com>
2026-06-01 10:44:19 +0200

    fix bg-main-layout radial

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -2,7 +2,7 @@ import clsx from 'clsx'
 import { CurrentProjects } from '../views/currentProjects'
 
 const mainClassName = clsx(
-  'min-h-screen bg-background p-6',
+  'min-h-screen bg-main-layout p-6',
   'sm:p-8',
   'md:p-10',
 )
